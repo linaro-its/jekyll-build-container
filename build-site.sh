3232f6906438668826514002ce7f031481d54fb5
Ciaran Moran <25228551+morancj@users.noreply.github.com>
2018-08-09 10:59:31 +0100

    Initial commit of Dockerfile

---
+++ build-site.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# If GEM_HOME isn't set, set a default
+if [ -z "$GEM_HOME" ]; then
+    export GEM_HOME=./.gems
+fi
+#
+# Make sure the gems directory exists
+if [ ! -d "$GEM_HOME" ]; then
+    mkdir $GEM_HOME
+fi
+#
+# Check we've got defined vars
+if [ -z "$JEKYLL_CONFIG" ]; then
+    echo "JEKYLL_CONFIG needs to be set"
+    exit 1
+fi
+if [ -z "$JEKYLL_ENV" ]; then
+    echo "JEKYLL_ENV needs to be set"
+    exit 1
+fi
+if [ "$JEKYLL_ENV" != "staging" ] && [ "$JEKYLL_ENV" != "production" ]; then
+    echo "JEKYLL_ENV must be set to 'staging' or 'production'"
+fi
+#
+# Install the bundle
+bundle Install
+#
+# Build the site
+bundle exec jekyll build --config "$JEKYLL_CONFIG" JEKYLL_ENV="$JEKYLL_ENV"
