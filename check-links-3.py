b10e89306ad1127b6f339d63d03f427338d1100e
Philip Colmer <philip.colmer@linaro.org>
2018-08-13 10:28:19 +0100

    Add link checker

--- check-links-3.py
+++ check-links-3.py
@@ -210,7 +210,7 @@ async def async_check_web(session, links):
 
 # Perform an async check of all of the web links we've collected then
 # build up a list of the affected files for the faulty links.
-def check_unique_links():
+async def check_unique_links():
     global status_count
     status_count = 1
 
@@ -226,12 +226,9 @@ def check_unique_links():
             verify_ssl=False,
             limit=500
         )
-        loop = asyncio.get_event_loop()
-        with aiohttp.ClientSession(connector=conn,
-                                   conn_timeout=60,
-                                   loop=loop) as session:
-            loop.run_until_complete(async_check_web(session, unique_links))
-        loop.close()
+        async with aiohttp.ClientSession(connector=conn,
+                                         conn_timeout=60) as session:
+            await async_check_web(session, unique_links)
         for p in file_link_pairs:
             # p[0] is the file path and p[1] is the URL.
             if (p[1] in html_cache_results and
@@ -306,7 +303,11 @@ def scan_directory(path, skip_list):
                     failed_links.append(r)
     if unique_links != []:
         # Can do a simple append here because these are all web failures
-        failed_links += check_unique_links()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        cul_result = loop.run_until_complete(check_unique_links())
+        loop.close()
+        failed_links += cul_result
     if failed_links != []:
         if output_file is not None:
             save_out = sys.stdout
@@ -352,7 +353,7 @@ if __name__ == '__main__':
     verbose = 0
     output_file = None
 
-    print("Linaro Link Checker (2018-02-01)")
+    print("Linaro Link Checker (2018-08-13)")
 
     if args.verbose is not None:
         verbose = args.verbose
